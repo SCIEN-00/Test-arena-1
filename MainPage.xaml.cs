3f9896f9ffc112d18c42cdcaeb7d2171bf619167
scien <lauri.kurs@outlook.com>
2020-02-27 21:45:02 +0200

    Revert "Revert "update""
    This reverts commit 1fcc18b437930175f255268bc8831fbf969c89c1.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Test_arena_1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DispatcherTimer Timer = new DispatcherTimer();
+            Timer.Tick += Timer_Tick;
+            Timer.Interval = new TimeSpan(0, 0, 1);
+            Timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            DateAndTime.Text = DateTime.Now.ToString("HH:mm:ss") + "\n" + DateTime.Now.ToString("dd.MM.yyy");
+        }
+
+        private void System_Tapped(object sender, RoutedEventArgs e)
+        {
+            if (System_b.IsSelected)
+            {
+                SystemSettingsPage_win.Visibility = Visibility.Collapsed;
+                System_b.IsSelected = false;
+            }
+            else
+            {
+                GpioTesting_win.Visibility = Visibility.Collapsed;
+                GPIO_b.IsSelected = false;
+                SystemSettingsPage_win.Visibility = Visibility.Visible;
+                System_b.IsSelected = true;
+                LiveCharts_win.Visibility = Visibility.Collapsed;
+                LiveCharts_b.IsSelected = false;
+                DataPlotting_win.Visibility = Visibility.Collapsed;
+                Data_plotting_b.IsSelected = false;
+            }
+        }
+
+        private void GPIO_Tapped(object sender, RoutedEventArgs e)
+        {
+            if (GPIO_b.IsSelected)
+            {
+                GpioTesting_win.Visibility = Visibility.Collapsed;
+                GPIO_b.IsSelected = false;
+            }
+            else
+            {
+                GpioTesting_win.Visibility = Visibility.Visible;
+                GPIO_b.IsSelected = true;
+                SystemSettingsPage_win.Visibility = Visibility.Collapsed;
+                System_b.IsSelected = false;
+                LiveCharts_win.Visibility = Visibility.Collapsed;
+                LiveCharts_b.IsSelected = false;
+                DataPlotting_win.Visibility = Visibility.Collapsed;
+                Data_plotting_b.IsSelected = false;
+            }
+        }
+    }
+}
